cc0b016d4f3a13e94c08828c0e1ae2f3820833fe
babsyinks <babawarunn@yahoo.com>
2020-09-18 13:35:39 +0100

    Updates

---
+++ precache-manifest.dc91d2ace0d6ed11a88a41f0f9aea8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cae2f8df71d9f0d17f25087b7cf083",
+    "url": "/JokesArena/index.html"
+  },
+  {
+    "revision": "edf6216cb4bdebd74fda",
+    "url": "/JokesArena/static/css/main.e11a11df.chunk.css"
+  },
+  {
+    "revision": "559107fdb2b6a7309a3d",
+    "url": "/JokesArena/static/js/2.20a09077.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/JokesArena/static/js/2.20a09077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf6216cb4bdebd74fda",
+    "url": "/JokesArena/static/js/main.cc8fa46f.chunk.js"
+  },
+  {
+    "revision": "13fc44ef06d937590c3a",
+    "url": "/JokesArena/static/js/runtime-main.1ecc780f.js"
+  }
+]);
